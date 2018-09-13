7e6f075b9e41088cf691748e457e8d3c6ae81d9d
kkobl <khrystyna052@gmail.com>
2018-09-13 16:12:39 +0300

    driver added

--- src/main/java/framework/utility/Driver.java
+++ src/main/java/framework/utility/Driver.java
@@ -1,4 +1,50 @@
 package framework.utility;
 
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.logging.Logger;
+
 public class Driver {
+
+    private IOSDriver driver;
+    final String APP_URL = "D:\\ProjectToTestTwo\\ApiDemos.apk";
+    final String APPIUM_URL = "http://127.0.0.1:4723/wd/hub";
+    final int TIME_OUT = 15;
+
+    private Driver() { }
+
+    private static Driver instance = new Driver();
+
+    public static Driver getInstance() {
+        return instance;
+    }
+
+    private ThreadLocal<IOSDriver> threadLocal = new ThreadLocal<IOSDriver>() {
+        @Override
+        protected IOSDriver initialValue() {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            if (driver == null) {
+                capabilities.setCapability("app", "");
+                capabilities.setCapability("platformName", "IOS");
+                capabilities.setCapability("platformVersion", "11.4");
+                capabilities.setCapability("deviceName", "iPhone X");
+                capabilities.setCapability("noReset", true);
+            }
+            return driver;
+        }
+    };
+
+    public IOSDriver getDriver() {return threadLocal.get(); }
+
+    public void removeDriver(){
+        driver = threadLocal.get();
+        try{
+            driver.manage().deleteAllCookies();
+            driver.quit();
+        }
+        finally {
+            threadLocal.remove();
+        }
+    }
 }
