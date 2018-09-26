57279646ef485189f356253432a8562b40a14b09
khrystyna <khrystyna052@gmail.com>
2018-09-26 02:31:41 -0700

    "added missing methods"

--- src/main/java/mozilla/pages/AllOtherPages.java
+++ src/main/java/mozilla/pages/AllOtherPages.java
@@ -1,6 +1,7 @@
 package mozilla.pages;
 
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -18,8 +19,11 @@ public class AllOtherPages extends HomePage {
     @FindBy(xpath = "//*[@name=\"TabLocationView.readerModeButton\"]")
     private WebElement BookingListBtn;
 
-    @FindBy(id = "ReaderModeBarView.listStatusButton")
+    @FindBy(xpath = "//*[@label = 'Add to Reading List']")
     private WebElement AddToBookingListBtn;
+
+    @FindBy(xpath = "//*[@label = 'Remove from Reading List']")
+    private WebElement DeleteFromReadingListBtn;
 
 
     //Actions
@@ -31,10 +35,23 @@ public class AllOtherPages extends HomePage {
         threeDotsBtn.click();
     }
 
-
-
     public void markAsBookingListItem() {BookingListBtn.click();}
 
-    public void addToBookingListClick() {AddToBookingListBtn.click();}
+    public void addToBookingListClick() {
+        try {
+            if (AddToBookingListBtn.isDisplayed()) {
+                AddToBookingListBtn.click();
+            }
+        }
+        catch (NoSuchElementException | StaleElementReferenceException e) {
+            System.out.println("Add to booking list button is not present, script continues to execute");
+        }
+    }
+
+    public Integer addToBookingListCount() {
+        int addCounter = 0;
+
+       return AddToBookingListBtn.isDisplayed()? addCounter++ : addCounter;
+    }
 
 }
